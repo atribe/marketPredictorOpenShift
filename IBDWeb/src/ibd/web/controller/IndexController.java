cb55d354e3c84bddaead476d86d89489bcb103de
Allan <Allan@10.13.14.169>
2014-02-02 23:37:27 -0700

    Changed web address from /index.do to /

--- IBDWeb/src/ibd/web/controller/IndexController.java
+++ IBDWeb/src/ibd/web/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/index")
+@RequestMapping("/")
 public class IndexController {
 	/**
 	 * 
